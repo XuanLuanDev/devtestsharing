7cc3dcf75372bd26b31294226e113f19f9520421
luannx <hyoko662@gmail.com>
2023-12-02 16:45:52 +0700

    update

--- src/app/pages/posts/post-detail/post-detail.component.ts
+++ src/app/pages/posts/post-detail/post-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthorService } from 'src/app/core/services/author.service';
 import { PostService } from 'src/app/core/services/post.service';
@@ -8,7 +8,7 @@ import { PostService } from 'src/app/core/services/post.service';
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.scss']
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit {
   post:any;
   slug:string ="";
   authors:any[]=[];
@@ -16,13 +16,16 @@ export class PostDetailComponent {
   constructor(private service:PostService,private route: ActivatedRoute,auths:AuthorService){
     this.authors =auths.authors;
     this.slug = this.route.snapshot.paramMap.get('slug')??""; 
-    service.getPost(this.slug).subscribe(t=>{
+   
+  }
+  ngOnInit(): void {
+    this.service.getPost(this.slug).subscribe(t=>{
       if(t.body.datas && t.body.datas.length >0)
       this.post =t.body.datas[0]
       this.author= this.getAuthor(this.post.author);
     })
   }
-  getAuthor(id:any){debugger
+  getAuthor(id:any){
     let a = this.authors.find(t=>t.id == id);
     if(a){
       return a;
